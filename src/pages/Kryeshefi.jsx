23418583ae04426920320c4340d81734a11c9d89
KreshnikKelmendi <kreshnik.kelmendi1994@gmail.com>
2023-11-22 16:03:33 -1200

    ecxcute

--- src/pages/Kryeshefi.jsx
+++ src/pages/Kryeshefi.jsx
@@ -13,7 +13,7 @@ const Kryeshefi = () => {
         </div>
       </div> */}
 
-    <div className="container-fluid py-lg-5">
+    {/* <div className="container-fluid py-lg-5">
         <div className="py-1 px-lg-5 ">
             <div className="d-flex kryeshefi">
                 <div className="col-lg-7">
@@ -43,7 +43,38 @@ const Kryeshefi = () => {
                     </div>
             </div>
         </div>
+        </div> */}
+
+        <div className="container-fluid py-lg-5 min-vh-100">
+        <div className="container-fluid px-lg-3">
+            <div className="d-flex kryeshefi justify-content-center align-items-center">
+            <div className="col-lg-6">
+                    <div className="section-title position-relative py-3 py-lg-0 mb-lg-3">
+                        <h5 className="text-primary text-uppercase historyHeading">KRYESHEFI EKZEKUTIV</h5>
+                        <h6>BARDH KRASNIQI</h6>
+                    </div>
+                    <p className="mb-4 col-lg-11 textOn3">
+                        Bardh Krasniqi është i lindur në Prishtinë në vitin 1990, dhe është rritur në lagjen Taslixhe.
+                        Shkollën Fillore e ka përfunduar në "Gjergj Fishta", kurse të Mesmen në "Kolegjin Ciragan" të Stamboll-it.
+                        Ka të përfunduar Administrimin e Biznesit në "Univesitetin Fatih" të Stambollit,"2007-2010", poashtu ka të përfunduar shkollën profesionale në AUK, atë të Akademisë Cisco's, ku u certifikua me titullin Inxhinier i Rrjetave Telekomunikuese.
+                        Bardhi ka punuar në kompaninë 3CIS për 9 vite, duke qenë pjesë e implementimit të rrjetave 3G dhe 4G për disa prej telefonive më të mëdha në botë, sic është VODAFONE UK, TELLABS, AT&T USA, etj.
+                        Është themelues i njërës prej kompanive më kreative në vend, "TREKUARTISTA" për tash e 10 vite, dhe ka menaxhuar shumë projekte brenda dhe jashtë shtetit.
+                        Në dhjetor të 2022 është përzgjedhur dhe mban pozitën e Kryeshefit në Ndërmarrjen Publike Lokale "Sport Marketing"
+                        Është pasionant i gjuhëve të huaja, dhe flet rrjedhshëm 5 gjuhë.
+                        Në Prishtinë dhe në Kosovë, njihej si njëri ndër talentët më të mëdhenjë të gjeneratës së tij në sportin e Basketbollit, ku për shumë vite djersiti fanelat e shumë klubeve vendore dhe ndërkombëtare duke qenë edhe pjesë e Përfaqësueses së Kosovës në Basketboll.
+                        <br /> <br />
+                        Bardhi është i martuar dhe babë i dy vajzave.
+                    </p>
+                </div>
+                <div className="col-lg-6 h-auto mt-2 mt-lg-0"  >
+                   
+                        <img className="col-12 staffPhoto" alt="" src={Bardhi} style={{objectFit: "cover"}} />
+                 
+                </div>
+                
+            </div>
         </div>
+    </div>
        </>
     )
 }
