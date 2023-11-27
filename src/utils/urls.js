471e2a0669db09ce8c8efff0cb29193670c5570d
KreshnikKelmendi <kreshnik.kelmendi1994@gmail.com>
2023-11-27 11:11:45 -1200

    as

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,4 +1,4 @@
 export const STRAPI_API_TOKEN = process.env.REACT_APP_API_TOKEN;
 
 export const API_URL = 
-       process.env.REACT_APP_API_URL || "https://www.nplsportmarketing.com";
+       process.env.REACT_APP_API_URL || "https://nplsportmarketing.onrender.com/";
