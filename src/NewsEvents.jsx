ccf172bf46bc3953069cc3c927b33705a8e6febb
KreshnikKelmendi <kreshnik.kelmendi1994@gmail.com>
2023-12-05 11:51:44 -1200

    ring

--- src/NewsEvents.jsx
+++ src/NewsEvents.jsx
@@ -2,9 +2,15 @@ import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import 'animate.css';
 import { fetchDataFromApi } from './utils/api';
+import { RingLoader } from 'react-spinners';
+
+// Import the component using React.lazy
+
 
 const NewsEvents = () => {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -18,8 +24,11 @@ const NewsEvents = () => {
 
       const sortedData = responseData.sort((a, b) => new Date(b.attributes.date) - new Date(a.attributes.date));
       setData(sortedData);
+      setLoading(false);
     } catch (error) {
       console.error('Error fetching data:', error);
+      setError('An error occurred while fetching data.');
+      setLoading(false);
     }
   };
 
@@ -39,39 +48,51 @@ const NewsEvents = () => {
             <h5 className="textOn text-primary text-uppercase">Të fundit</h5>
           </div>
           <div className="row g-5">
-            {data?.map((item) => (
-              <div className="col-lg-4" key={item.id}>
-                <div className="blog-item bg-light rounded overflow-hidden h-100">
-                  <div className="blog-img position-relative overflow-hidden">
-                    <img className="blogImage" src={item?.attributes?.img?.data[0]?.attributes?.url} alt="" />
-                  </div>
-                  <div className="p-4">
-                    <div className="d-flex mb-3">
-                      <small className="text-success textOn">
-                        <i className="far fa-calendar-alt text-primary me-2"></i>
-                        {item?.attributes?.date}
-                      </small>
+            {loading ? (
+              <div className="text-center">
+                <RingLoader color="#007BFF" loading={loading} size={150} />
+              </div>
+            ) : (
+              data?.map((item) => (
+                <div className="col-lg-4" key={item.id}>
+                  <div className="blog-item bg-light rounded overflow-hidden h-100">
+                    <div className="blog-img position-relative overflow-hidden">
+                      <img
+                        className="blogImage"
+                        src={item?.attributes?.img?.data[0]?.attributes?.url}
+                        alt={item?.attributes?.title || 'Image'}
+                      />
                     </div>
-                    <h5 className="mb-3 textOn text-uppercase">{item?.attributes?.title}</h5>
-                    <Link
-                      onClick={() =>
-                        window.scrollTo({
-                          top: 0,
-                          left: 0,
-                        })
-                      }
-                      to={`/news/${item.id}`}
-                      className=" linkHover textOn text-decoration-none"
-                    >
-                      Lexo më shumë<i className="mx-2 bi bi-arrow-right"></i>
-                    </Link>
+                    <div className="p-4">
+                      <div className="d-flex mb-3">
+                        <small className="text-success textOn">
+                          <i className="far fa-calendar-alt text-primary me-2"></i>
+                          {item?.attributes?.date}
+                        </small>
+                      </div>
+                      <h5 className="mb-3 textOn text-uppercase">{item?.attributes?.title}</h5>
+                      <Link
+                        onClick={() =>
+                          window.scrollTo({
+                            top: 0,
+                            left: 0,
+                          })
+                        }
+                        to={`/news/${item.id}`}
+                        className="linkHover textOn text-decoration-none"
+                      >
+                        Lexo më shumë<i className="mx-2 bi bi-arrow-right"></i>
+                      </Link>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))
+            )}
           </div>
         </div>
       </div>
+
+      {error && <p style={{ color: 'red' }}>{error}</p>}
     </>
   );
 };
--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,9 +1,15 @@
+import { Suspense, lazy } from "react";
 import NewsEvents from "../NewsEvents";
 
+const LazyNewsEvents  = lazy(() => import('../NewsEvents'));
+
 const News = () =>{
+
     return(
         <>
-          <NewsEvents />
+          <Suspense fallback={<div>Loading...</div>}>
+            <LazyNewsEvents />
+          </Suspense>
         </>
     )
 }
