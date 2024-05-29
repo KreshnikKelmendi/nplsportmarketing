5305498137e783a2ffef8cd8377a25cbc1b868ed
KreshnikKelmendi <kreshnik.kelmendi1994@gmail.com>
2024-05-28 12:20:46 -1200

    shpallja3

--- src/dataPova/shpalljet.js
+++ src/dataPova/shpalljet.js
@@ -1,8 +1,17 @@
 import image1 from "../img/pishina-e-germise.jpg";
 import file1 from "../img/shpallje_-_oferte_per_hapesirat_ne_pishine_-_final (1).pdf"
 import file2 from "../img/Njoftim per shtyerje te afatit per aplikim.pdf"
+import file3 from "../img/Njoftimi per kompanine fituese te tenderit 2022 1.pdf"
 
 const shpalljet = [
+    {
+        id:3,
+        title: "NJOFTIM PËR KOMPANINË FITUESE",
+        date: "28 Maj 2024",
+        name: "MENAXHIMI I PISHINES DHE SHËRBIMET HOTELIERE NË PISHINËN E GËRMISË NË PRISHTINË",
+        photo: image1,
+        pdfFile: file3
+    },
     {
         id:2,
         title: "NJOFTIM PËR SHTYERJE TË AFATIT PËR APLIKIM",
